20d12a7769342a13dc599faec0ec697519dc848e
Griffin Riley <rileyg@oregonstate.edu>
2023-06-10 15:12:27 -0700

    added fake data to init.sql and updated schema

--- data/init.sql
+++ data/init.sql
@@ -30,8 +30,8 @@ CREATE TABLE `userscourses` (
 CREATE TABLE `assignments` (
   `id` mediumint NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
-  `points` mediumint NOT NULL,
-  `dueDate` varchar(255) NOT NULL,
+  `points` float,
+  `dueDate` varchar(255),
   `courseid` mediumint NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`courseid`) REFERENCES `courses` (`id`)
@@ -40,11 +40,52 @@ CREATE TABLE `assignments` (
 CREATE TABLE `submissions` (
   `id` mediumint NOT NULL AUTO_INCREMENT,
   `timestamp` varchar(255) NOT NULL,
-  `grade` mediumint NOT NULL,
-  `file` varchar(255) NOT NULL,
+  `grade` float,
+  `file` mediumblob,
   `userid` mediumint NOT NULL,
   `assignmentid` mediumint NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`userid`) REFERENCES `users` (`id`),
   FOREIGN KEY (`assignmentid`) REFERENCES `assignments` (`id`)
 );
+
+INSERT INTO users SET
+  name = `Dave Student`,
+  email = `davest@oregonstate.edu`,
+  password = `hunter2`,
+  role = `student`;
+
+INSERT INTO users SET
+  name = `Jane Admin`,
+  email = `janead@oregonstate.edu`,
+  password = `hunter2`,
+  role = `admin`;
+
+INSERT INTO users SET
+  name = `John Instructor`,
+  email = `johnin@oregonstate.edu`,
+  password = `hunter2`,
+  role = `instructor`;
+
+INSERT INTO courses SET
+  subjectcode = `CS`,
+  number = 422,
+  title = `introductory smart fridge programming`
+  term = `sp22`
+  instructorid = 3;
+
+INSERT INTO userscourses SET
+    userid = 1,
+    courseid = 1;
+
+INSERT INTO assignments SET
+  title = `project 1`,
+  points = 50,
+  dueDate = "2022-06-14T17:00:00-07:00",
+  courseid = 1;
+
+INSERT INTO submissions SET
+  timestamp = "2022-06-14T17:00:00-06:59",
+  grade = 43.5,
+  userid = 1,
+  assignmentid = 1;
