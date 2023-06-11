47d7c243b58ef1ec71db32c91d4d92b42c9ff073
Nikomixo <kevint0218@outlook.com>
2023-06-10 17:04:05 -0700

    create user

--- components/user.js
+++ components/user.js
@@ -1,3 +1,8 @@
+const { extractValidFields } = require('../lib/validation');
+const bcrypt = require('bcryptjs');
+
+const db = require('../lib/connection');
+
 const UserSchema = {
     name: { required: true },
     email: { required: true },
@@ -5,3 +10,31 @@ const UserSchema = {
     role: { required: true }
 }
 exports.UserSchema = UserSchema;
+
+async function checkIfEmailExists(email) {
+    const [results] = await db.query(
+        "SELECT * FROM users WHERE email = ?",
+        [email]
+    )
+    console.log(`email: ${email}, results: ${results.length > 0}`);
+    return results.length > 0;
+}
+exports.checkIfEmailExists = checkIfEmailExists;
+
+async function insertNewUser(user) {
+    const validatedUser = extractValidFields(
+        user,
+        UserSchema
+    );
+
+    validatedUser.password = await bcrypt.hash(validatedUser.password, 8);
+
+    console.log(validatedUser);
+
+    const [results] = await db.query(
+        "INSERT INTO users SET ?",
+        validatedUser
+    );
+    return results.insertId;
+}
+exports.insertNewUser = insertNewUser;
